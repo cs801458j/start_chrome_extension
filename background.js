c29f829fe34e983be0d9cf2f41798c464b0995a8
eunjeong <cs801458j@gmail.com>
2018-05-01 23:44:59 +0900

    chrome extension works

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
-{ "name" : "GGetting Started Example By Eunjeong" , "version" : "1.0" , "description" : "확장 빌드!" , "background" : { "scripts" : [ "background.js" ], "persistent" : false }, "manifest_version" : 2 }
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+        console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'developer.chrome.com'},
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
